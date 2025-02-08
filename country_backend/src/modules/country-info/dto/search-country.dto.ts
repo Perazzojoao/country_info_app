26eaa4f0a335b7970ae964f64d9663ea5e35f514
Perazzojoao <perazzojoao@gmail.com>
2025-02-08 18:57:03 -0300

    Adding dto for search country by name

--- country_backend/src/modules/country-info/dto/search-country.dto.ts
+++ country_backend/src/modules/country-info/dto/search-country.dto.ts
@@ -1,8 +1,16 @@
-import { IsNotEmpty, MaxLength } from "class-validator";
+import { IsNotEmpty, MaxLength, MinLength } from "class-validator";
 
-export class SearchCountryDto {
+export class SearchCountryByCodeDto {
 
   @IsNotEmpty()
-  @MaxLength(10, { message: 'Country code can not be longer than 10 characters' })
+  @MaxLength(3, { message: 'Country code can not be longer than 3 characters' })
   country_code: string;
+}
+
+export class SearchCountryByNameDto {
+
+  @IsNotEmpty()
+  @MinLength(3, { message: 'Country name can not be shorter than 3 characters' })
+  @MaxLength(20, { message: 'Country name can not be longer than 20 characters' })
+  country_name: string;
 }
