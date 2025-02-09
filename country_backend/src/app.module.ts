aa46ab579a9ac5b508b2112efb117e3a16a9c5d1
Perazzojoao <perazzojoao@gmail.com>
2025-02-09 00:26:06 -0300

    Applaying logger and exeption filter

--- country_backend/src/app.module.ts
+++ country_backend/src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
+import { ConsoleLogger, Module } from '@nestjs/common';
 import { AvailableCountriesModule } from './modules/available-countries/available-countries.module';
 import { ApiModule } from './api/api.module';
 import { CountryInfoModule } from './modules/country-info/country-info.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from './resources/interceptors/logger.interceptor';
+import { HttpExceptionFilter } from './resources/filters/http-exeption.filter';
 
 @Module({
   imports: [AvailableCountriesModule, ApiModule, CountryInfoModule],
-  controllers: [],
-  providers: [],
+  providers: [
+     ConsoleLogger,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
